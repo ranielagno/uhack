eacdcbe5c2b912d07bf92bd6ece88ccb19016a8d
Ariel <bscpev42017@gmail.com>
2017-12-03 00:25:39 +0800

    drawables

--- app/src/main/java/com/ranielagno/uhack/ActivityHomepage.java
+++ app/src/main/java/com/ranielagno/uhack/ActivityHomepage.java
@@ -1,10 +1,12 @@
 package com.ranielagno.uhack;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Makati on 12/2/2017.
@@ -16,7 +18,19 @@ public class ActivityHomepage extends Fragment {
         View rootView = inflater.inflate(R.layout.activity_homepage, container, false);
 
 
-    return rootView;
+        Button btn_car_reg = rootView.findViewById(R.id.pracInvButton);
 
+        btn_car_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return rootView;
     }
+
 }
